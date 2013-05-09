5894552b17e0d8596c7cac55332d8a59d9bab19b
Dario Seyb <dario.seyb@gmail.com>
2013-05-09 18:22:49 +0200

    Revert "Initial commit"
    This reverts commit ef14bb55f3398326525623b6d10ba357fa66ce2e.

--- SoftwareRasterizer/SoftwareRasterizer/Intersection.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "SimpleMath.h"
-
-struct Intersection
-{
-	DirectX::SimpleMath::Vector3 position;
-	DirectX::SimpleMath::Vector3 normal;
-	DirectX::SimpleMath::Color color;
-};
